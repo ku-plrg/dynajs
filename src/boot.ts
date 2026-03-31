b668dd9f023266dc1837065e670c303e28e434ad
Minseok Choe <choems@outlook.com>
2026-03-31 14:26:52 +0900

    fix: error

--- src/boot.ts
+++ src/boot.ts
@@ -8,10 +8,11 @@ export function checkAnalysisHooks(fullOpt: boolean): CallbackHint | undefined {
   if (!analysis) return undefined;
 
   const tags: CallbackHint = { ...callbackHintEmpty }; 
-  const keys = Object.keys(callbackHintEmpty) as (keyof typeof callbackHintEmpty)[];
-  for (const callbackName of keys) {
-    if (callbackName in analysis) { tags[callbackName] = true; }
-    else { warn(`unknown analysis callback name \`${callbackName}\` detected. Is this a typo?`); }
+  const validKeys = Object.keys(callbackHintEmpty) as (keyof typeof callbackHintEmpty)[];
+  const live = new Set(Object.keys(analysis));
+  for (const maybeCallbackName of live) {
+    if (validKeys.includes(maybeCallbackName as keyof CallbackHint)) { tags[maybeCallbackName as keyof CallbackHint] = true; }
+    else { warn(`unknown analysis callback name \`${maybeCallbackName}\` detected. Is this a typo?`); }
   }
   return tags;
 }
