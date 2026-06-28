17c8a444307fc08709d581bdf43be324c6cca615
Minseok Choe <choems@outlook.com>
2026-06-28 21:14:53 +0900

    fix: model

--- analyses/flow/spec/AO__GetV.manual.ts
+++ analyses/flow/spec/AO__GetV.manual.ts
@@ -1,7 +1,8 @@
 import type { SpecRuntime, Lifted, Unlifted, Primitive } from "../type.js";
 
+import { AO__ToObject } from "./AO__ToObject.js";
+
 export function AO__GetV($: SpecRuntime, V: Lifted<unknown>, P: Lifted<unknown>): Lifted<unknown> {
-  // @ts-ignore $.value(P) can be used as key.
-  return $.default($.value(V)[$.value(P)], [V, P]);
-  // TODO check if this transformation is correct? 
+  const O = AO__ToObject($, V);
+  return $.get(O, P);
 }
